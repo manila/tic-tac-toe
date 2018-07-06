b8e51327fc176749ad96f48511f7997ec0fbd76b
manila <manila@users.noreply.github.com>
2018-07-05 18:22:32 -0600

    outlining

--- main.c
+++ main.c
@@ -1,28 +1,37 @@
 
+#include <stdio.h>
 
-int	game_loop(void)
+void	place_piece(int position_number, int *bitboard)
+{
+	bitboard = (bitboard | (((int) 1) << (position_number - 1)));	
+}
+
+void	game_loop(void)
 {
 	int game_over = 0;
 	int current_player = 0;
 	char player_piece[] = "XO";
 	int bitboard[2];
+	int position_number;
 	
 	while (!game_over)
 	{
 		pretty_print_board(bitboard[0], bitboard[1]);
 	
-		printf("Player %d: Enter Desired Position Number: ", current_player + 1);
-		scanf("%d", position);
+		printf("\nPlayer %d: Enter Desired Position Number: ", current_player + 1);
+		scanf("%d", &position_number);
 	
-		place_piece(position, &bitboard[player_piece], player_piece[current_player]);
+		place_piece(position, &bitboard[current_player]);
 
-		check_win_horizontal();
-		//check_win_verticle();
-		check_win_diag_left();
-		//check_win_diag_right();
+		if (check_win_horizontal() &
+		  //check_win_verticle()   &
+		 //check_win_diag_right()  &
+		    check_win_diag_left())
+		{
+			printf("\nPlayer %d Won!\n", current_player + 1);
+			game_over = 1;		
+		}
 	}
-
-	return (0);
 }
 
 int	main(void)
